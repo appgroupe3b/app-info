3ada7cffad7d1a496b9bc18b5266ec4cc9e432b9
AliAmraniHanchi <a.amrani1997@gmail.com>
2017-12-14 10:45:18 +0100

    footer

---
+++ footer/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="footer.css">
+<title>Footer</title>
+</head>
+    	<footer>
+
+    		<div id="socials">
+
+    			<a href="#" class="social"><img src="mail.png" alt="social"/></a>
+                <a href="#" class="social"><img src="twi.png" alt="social"/></a>
+                <a href="#" class="social"><img src="face.png" alt="social"/></a>
+
+        	</div>
+
+        	<div class="copyright">&copy;  2017 All Rights Reserved</div>
+
+    	</footer>
+
+</html>

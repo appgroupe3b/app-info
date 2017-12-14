4a203d043061d747fa4bcf0b27bc6c408262bb38
anauxe <33418144+anauxe@users.noreply.github.com>
2017-12-14 11:29:21 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,13 @@
+<?php
+  $dbname = 'bdd1';
+  $host='localhost:3306';
+  $user='root';
+  $pass= 'root';
+
+    $db = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pass");
+    $db->query("SET NAMES UTF8");
+    $db->exec("SET NAMES UTF8");
+    $link = mysqli_connect("$host", "$user", "$pass", "$dbname");
+
+
+?>

41dca5777e7a34fb57636b4327a560dc5e6770f4
Eloise Almeida <almeida.eloise@gmail.com>
2017-12-14 10:55:47 +0100

    controleurs

--- catalogue/controleur_catalogue01.php
+++ catalogue/controleur_catalogue01.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 //On se connecte à la BDD
 try
 {
@@ -13,19 +10,15 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-include 'modele_catalogue01.php';
+include '../modele/modele_catalogue01.php';
 
-$donnees = recuperer_donnees($bdd);
-
-
-/*if (isset($_GET['id_piece']))
+if (isset($_GET['id_catalogue']))
 {
-		/*$_GET['id_piece'] = (int) $_GET['id_piece'];*/
-	
-		/*$_GET['id_piece'] = $id_piece();
-		
-		$capteurs = recuperer_capteurs($bdd, $id_piece); 
+    $id_catalogue = $_GET['id_catalogue'];
+    echo $id_catalogue;
+    
+    $donnees = recuperer_donnees($bdd, $id_catalogue);
 }
-*/
+
 
 ?>
